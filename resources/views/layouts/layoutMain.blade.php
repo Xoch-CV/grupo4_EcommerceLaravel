3eea0e64b1d5d9e1418fa8f126c6ae0fb72cdd97
Camilo <caperezgonz@gmail.com>
2019-12-10 18:34:44 -0300

    Removed slash

--- resources/views/layouts/layoutMain.blade.php
+++ resources/views/layouts/layoutMain.blade.php
@@ -3,30 +3,30 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    
-    <link href="{{asset ('/css/fontawesome/css/all.css')}}" rel="stylesheet">
+
+    <link href="{{asset ('css/fontawesome/css/all.css')}}" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap-4.3.1-dist/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,900&display=swap" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <!--<link href="{{ asset('css/styleregister.css') }}" rel="stylesheet">-->
-        
+
     <title>{{ config('app.name', 'Ticket') }}</title>
 </head>
     <body>
-        
+
         @include('partials.navbar')
         @yield('content')
         @include('partials.footer')
-        
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
